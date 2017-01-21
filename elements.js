72fad60665374f689947a2debd100425d5af2636
rbhat35 <rbhat37@yahoo.com>
2017-01-21 13:05:44 -0500

    Better Functionality

--- elements.js
+++ elements.js
@@ -1,6 +1,6 @@
 function myFunction(data) {
-    city = data.location.city;
-    state = data.location.state;
+    var city = data.location.city;
+    var state = data.location.state;
     $("#yourCity").text(city);
     $("#yourState").text(state);
 
@@ -15,6 +15,18 @@ function myFunction(data) {
 function printStuff(data) {
     console.log(data);
     $("#temperature").text(data.current_observation.feelslike_f);
+    //$("#humidity").text(data.current_observation);
+
+    var rain = data.current_observation.precip_today_in;
+    var out = "";
+    if(rain > 0.01) {
+        out = "Yup!";
+    }
+    else {
+        out = "Nope!";
+    }
+
+    $("#chanceOfRain").text(out);
 
 
 }
