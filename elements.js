ee35155d787f4603b95ef7935a2e48bafe4cdb2a
rbhat35 <rbhat37@yahoo.com>
2017-01-21 14:15:55 -0500

    Fixed bugs

--- elements.js
+++ elements.js
@@ -1,5 +1,9 @@
 function main(){
 
+    $("#zipcodeText").keyup(function(event){
+        if(event.keyCode == 13){
+            $("#button").click();
+        }   });
     var buttonPress = document.getElementById("button");
     buttonPress.addEventListener("click", getData);
 
@@ -15,23 +19,18 @@ function myFunction(data) {
     $("#yourCity").text(city);
     $("#yourState").text(state);
 
-    //console.log(data);
-    //console.log($.getJSON("http://api.wunderground.com/api/b819c4c776539987/geolookup/q/30332.json"));
-    //console.log(curr_obs.feelslike_f);
-
-
     $.getJSON("http://api.wunderground.com/api/b819c4c776539987/conditions/q/" + state + "/" + city + ".json", "", printStuff)
 }
 
 function printStuff(data) {
     console.log(data);
-    $("#temperature").text(data.current_observation.feelslike_f);
+    $("#temperature").text(data.current_observation.feelslike_f + " degrees Fahrenheit.");
     //$("#humidity").text(data.current_observation);
 
     var rain = data.current_observation.precip_today_in;
     var out = "";
     if(rain > 0.01) {
-        out = "Yup!";
+        out = ("Yup, you're looking at about " + rain + " inches!");
     }
     else {
         out = "Nope!";
